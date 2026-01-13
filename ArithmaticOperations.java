8802a5d7fa2e8c6f602e7312761ca0ec09924c5b
Harsh9761 <harshmathura8@gmail.com>
2026-01-13 15:06:55 +0530

    Code added

---
+++ ArithmaticOperations.java
@@ -0,0 +1,9 @@
+public class ArithmaticOperations{
+    public static void main(String[] args) {
+        
+    }
+
+    public static void sum(int a,int b){
+        System.out.println("Sum is: "+a+b);
+    }
+}
